6be4300ede6847493b80208700fa0710272af1be
Mario <marioheras02@gmail.com>
2023-02-21 20:19:47 +0100

    gitignore angular

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpeedrunsAngular.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public bool Login(string username, string password)
+        {
+            // Busco en base de datos este username y esta password
+            
+            return username == "admin" && password == "admin";
+        }
+        [HttpPost]
+        public bool Register(string username, string password)
+        {
+            // Si el usuario no esta registrado, lo añadimos
+
+            return username == "admin" && password == "admin";
+        }
+    }
+}
