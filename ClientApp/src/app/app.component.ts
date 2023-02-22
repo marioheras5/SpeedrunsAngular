d85b5a8d37850a34e89b1f2f7f546032cdd4b9c9
Mario <marioheras02@gmail.com>
2023-02-22 14:51:21 +0100

    cambio favicon con tema

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -5,5 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  constructor() {
+    this.changeFavicon();
+  }
   title = 'app';
+  assetDark: string = 'assets/favicon-dark.png';
+  assetLight: string = 'assets/favicon.png';
+
+  changeFavicon() {
+    var link: any = document.querySelector("link[rel*='icon']");
+    if (link == null) return;
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      link.href = this.assetDark;
+    } else {
+      link.href = this.assetLight;
+    }
+  }
 }
