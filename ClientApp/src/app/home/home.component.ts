eef03bd8bb665b8281a48fff8ad79172df07c27f
Mario <marioheras02@gmail.com>
2023-02-27 20:05:05 +0100

    games

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -7,13 +7,20 @@ import { HttpClient, HttpParams } from '@angular/common/http';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  public games: Game[] | undefined;
   constructor( private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
     this.getGames(0, 9);
   }
   getGames(page: number, size: number) {
     var params = new HttpParams().set('page', page.toString()).set('size', size.toString());
-    this.http.get<any>(this.baseUrl + 'api/games', params).subscribe(result => {
-      console.log(result);
+    this.http.get<Game[]>(this.baseUrl + 'api/games/GetGames', { params: params }).subscribe(result => {
+      this.games = result;
     }, error => console.error(error));
   }
+}
+interface Game {
+  id: number;
+  name: string;
+  shortName: string;
+  img: string;
 }
